3437f79439283755d17c1b5a5793e9715d9b9eb8
Alla Berkana <88716417+AllaBerkana@users.noreply.github.com>
2021-08-20 18:20:32 +0300

    Мод.окно

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     //переменные для кнопок вверх.вниз
     var counterUp = $('.counter_up');
     var counterDown = $('.counter_down');
+
     //при наведении мышкой по этажу, вызываем функцию
     floorPath.on('mouseover', function () {
         //очищение от класса, который придает стили этажу
@@ -14,7 +15,6 @@ $(document).ready(function () {
         currentFloor = $(this).attr('data-floor');
         //присваиваем counter текст из currentFloor, т.е. высветится число на какой этаж мы нажали
         $('.counter').text(currentFloor);
-
     });
 
     counterUp.on('click', function () {
@@ -53,4 +53,88 @@ $(document).ready(function () {
         }
     });
 
+    /* ------Modal Window */
+
+    var modal = $('.modal');
+    var buttonPrimary = $('.button__primary');
+    var modalCloseBtn = $('.modal__close-btn');
+
+    //функция переключения классов к мод.окну
+    function modalToggle() {
+        modal.toggleClass('modal_open');
+    };
+
+    //по клику вызываем функцию окрытия/закрытия мод.окна
+    floorPath.on('click', modalToggle);
+    buttonPrimary.on('click', modalToggle);
+    modalCloseBtn.on('click', modalToggle);
+
+
+    /*----- JS */
+    /*
+    const modal = document.querySelector('.modal');
+    const buttonPrimary = document.querySelector('.button__primary');
+    const modalCloseBtn = document.querySelector('.modal__close-btn');
+
+    const modalToggle = (event) => {
+        event.preventDefault();
+        modal.classList.toggle('modal_open');
+    };
+
+    floorPath.on('click', modalToggle);
+    buttonPrimary.addEventListener('click', modalToggle);
+    modalCloseBtn.addEventListener('click', modalToggle);
+    */
+
+
+    /* ------ Flats JS */
+
+        const flatsPath = document.querySelectorAll('.flats path');
+        const flatItem = document.querySelectorAll('.flat__item');
+
+        const chooseFlatImg = () => {
+            flatsPath.forEach((img) => {
+
+                img.addEventListener('click', (event) => {
+                    const target = event.target;
+                    const flatImg = img.dataset.flat;
+                    if (target.classList.contains('current-flat')) {
+                        target.classList.remove('current-flat');
+                    } else {
+                        target.classList.add('current-flat');
+                    }
+
+                    flatItem.forEach((room) => {
+                        const roomNum = room.dataset.flatId;
+                        if (flatImg === roomNum) {
+                            room.classList.toggle('current-item');
+                        }
+                    });
+                });
+            });
+        };
+
+        const chooseFlat = () => {
+            flatItem.forEach((item) => {
+                item.addEventListener('click', () => {
+
+                    if (item.classList.contains('current-item')) {
+                        item.classList.remove('current-item');
+                    } else {
+                        item.classList.add('current-item');
+                    }
+
+                    flatsPath.forEach((img) => {
+                        if (img.dataset.flat === item.dataset.flatId) {
+                            img.classList.toggle('current-flat');
+                        }
+                    })
+                })
+            })
+        };
+
+        chooseFlatImg();
+        chooseFlat();
+
 });
+
